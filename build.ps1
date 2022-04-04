a00528d54c537b6b3240a41859293ddc2119edbc
CaptM44 <mmorgan4x@gmail.com>
2022-04-04 16:30:04 -0400

    font fix

--- build.ps1
+++ build.ps1
@@ -16,10 +16,15 @@ Get-ChildItem  -Filter *.html | Copy-Item -Destination dist
 # copy libraries
 md dist/node_modules/bootstrap/dist/css
 Copy-Item node_modules/bootstrap/dist/css/bootstrap.min.css -Destination dist/node_modules/bootstrap/dist/css/bootstrap.min.css
-md dist/node_modules/bootstrap-icons/font
+
+md dist/node_modules/bootstrap-icons/font/fonts
 Copy-Item node_modules/bootstrap-icons/font/bootstrap-icons.css -Destination dist/node_modules/bootstrap-icons/font/bootstrap-icons.css
+Copy-Item node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff -Destination dist/node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff
+Copy-Item node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2 -Destination dist/node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2
+
 md dist/node_modules/jquery/dist
 Copy-Item node_modules/jquery/dist/jquery.slim.min.js -Destination dist/node_modules/jquery/dist/jquery.slim.min.js
+
 md dist/node_modules/bootstrap/dist/js
 Copy-Item node_modules/bootstrap/dist/js/bootstrap.bundle.min.js -Destination dist/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 
