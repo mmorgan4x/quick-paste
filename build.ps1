ed261886c6642223d80c8e6e305d224e3b4937d5
CaptM44 <mmorgan4x@gmail.com>
2022-04-04 15:32:42 -0400

    build updates

---
+++ build.ps1
@@ -0,0 +1,27 @@
+
+
+# build
+npm run build
+
+# remove old files
+Get-ChildItem -Path dist | Remove-Item -recurse 
+
+# copy files
+Copy-Item manifest.json -Destination dist
+Copy-Item images -Destination dist -Recurse
+Get-ChildItem  -Filter *.js | Copy-Item -Destination dist
+Get-ChildItem  -Filter *.css | Copy-Item -Destination dist
+Get-ChildItem  -Filter *.html | Copy-Item -Destination dist
+
+# copy libraries
+md dist/node_modules/bootstrap/dist/css
+Copy-Item node_modules/bootstrap/dist/css/bootstrap.min.css -Destination dist/node_modules/bootstrap/dist/css/bootstrap.min.css
+md dist/node_modules/bootstrap-icons/font
+Copy-Item node_modules/bootstrap-icons/font/bootstrap-icons.css -Destination dist/node_modules/bootstrap-icons/font/bootstrap-icons.css
+md dist/node_modules/jquery/dist
+Copy-Item node_modules/jquery/dist/jquery.slim.min.js -Destination dist/node_modules/jquery/dist/jquery.slim.min.js
+md dist/node_modules/bootstrap/dist/js
+Copy-Item node_modules/bootstrap/dist/js/bootstrap.bundle.min.js -Destination dist/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+
+# create .zip
+Compress-Archive -Path dist/* -DestinationPath build.zip -Force
